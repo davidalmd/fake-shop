6626ce5a1b205368df8b087250985477450af728
David Almeida <david.almeida@grupomoura.com>
2025-10-29 17:54:56 -0300

    alterando arquivos do dockerfile e entrypoint.sh e criando o deployment do kubernetes

--- src/index.py
+++ src/index.py
@@ -20,9 +20,9 @@ metrics.register_endpoint('/metrics')
 
 # Configuração do banco de dados
 db_host = os.getenv('DB_HOST', 'localhost')
-db_user = os.getenv('DB_USER', 'ecommerce')
-db_password = os.getenv('DB_PASSWORD', 'Pg1234')
-db_name = os.getenv('DB_NAME', 'ecommerce')
+db_user = os.getenv('DB_USER', 'admin')
+db_password = os.getenv('DB_PASSWORD', 'fake-shop-password')
+db_name = os.getenv('DB_NAME', 'fake_shop_db')
 db_port = os.getenv('DB_PORT', 5432)
 
 db_url = f"postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
